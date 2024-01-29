bb3e97a2db0d185ed4d887a53fa8a374ff6f3ad3
Federico Zanoni <153846199+Federicozan02@users.noreply.github.com>
2024-01-29 16:27:01 +0100

    Update MappaCorrelazione.py

--- Script/MappaCorrelazione.py
+++ Script/MappaCorrelazione.py
@@ -4,6 +4,7 @@ import seaborn as sns
 
 file_name = "insurance.csv"
 
+#Inserisco i dati del csv in un dataframe
 try:
 	insurance = pd.read_csv(file_name)
 except:
@@ -11,9 +12,11 @@ except:
 
 dati = pd.DataFrame(insurance)
 
+#Limito i valori alle colonne fumatori, bmi e rate
 insurance_data = dati[['smoker', 'bmi', 'charges']]
 insurance_data['smoker'] = insurance_data['smoker'].map({'yes': 1, 'no': 0})
 
+#creo la mappa di correlazione con i valori limitati
 correlation_matrix = insurance_data.corr()
 sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm")
 plt.title("Matrice di Correlazione")
